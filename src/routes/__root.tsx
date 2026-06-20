b543b650b01591e3213e02361ee5500f6d2054ab
Dennise Catolos <dentolos19@gmail.com>
2026-06-21 00:40:44 +0800

    Added website icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,7 +15,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "NYPTech",
+        title: "NYP Technopreneurship Club",
       },
     ],
     links: [
